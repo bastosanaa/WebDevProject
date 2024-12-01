d3bc2e953f308da1177fafb55ac07ee9424349af
Giulia de Bem <giuliadbem@gmail.com>
2024-12-01 11:38:09 -0300

    Adiciona Router no frontend

---
+++ frontend/src/components/pages/LoginPage.tsx
@@ -0,0 +1,16 @@
+import LoginForm from "../LoginForm";
+import logo from "../../assets/logo.png"; // A logo já está configurada
+
+const LoginPage = () => (
+  <div className="w-full max-w-lg p-12 rounded-2xl shadow bg-neutral-200 opacity-70">
+    <div className="flex flex-col gap-5">
+      <img src={logo} alt="Bloom Logo" className="logo" />
+      <p className="signup-text">
+        Não possui conta? <a href="#">Cadastre-se</a>
+      </p>
+      <LoginForm />
+    </div>
+  </div>
+);
+
+export default LoginPage;
--- frontend/src/components/pages/MainPage.tsx
+++ frontend/src/components/pages/MainPage.tsx
@@ -1,53 +1,52 @@
 import React, { useState } from "react";
-import './MainPage.css';
+import "./MainPage.css";
 import NavBar from "../widgets/NavBar";
 import SideBar from "../widgets/SideBar";
 import Tasks from "../features/todoList/Tasks";
 import Task from "../features/todoList/Task";
 import NewTask from "../features/todoList/NewTask";
 
-
 const MainPage: React.FC = () => {
-    const [selectedTask, setSelectedTask] = useState<string | null>(null);
-    const [showTasks, setShowTasks] = useState<boolean>(true);
-    const [showNewTask, setShowNewTask] = useState<boolean>(false);
+  const [selectedTask, setSelectedTask] = useState<string | null>(null);
+  const [showTasks, setShowTasks] = useState<boolean>(true);
+  const [showNewTask, setShowNewTask] = useState<boolean>(false);
 
-    const handleTaskClick = (task: string) => {
-        setSelectedTask(task);
-        setShowTasks(false);
-        setShowNewTask(false);
-    };
+  const handleTaskClick = (task: string) => {
+    setSelectedTask(task);
+    setShowTasks(false);
+    setShowNewTask(false);
+  };
 
-    const handleCloseDetail = () => {
-        setSelectedTask(null);
-        setShowTasks(true);
-        setShowNewTask(false);
-    };
+  const handleCloseDetail = () => {
+    setSelectedTask(null);
+    setShowTasks(true);
+    setShowNewTask(false);
+  };
 
-    const handleAddTask = () => {
-        setShowNewTask (true);
-        setShowTasks(false);
-        setSelectedTask(null);
-    }
+  const handleAddTask = () => {
+    setShowNewTask(true);
+    setShowTasks(false);
+    setSelectedTask(null);
+  };
 
-    const handleCloseNewTask = () => {
-        setShowNewTask(false);
-        setShowTasks(true);
-    }
+  const handleCloseNewTask = () => {
+    setShowNewTask(false);
+    setShowTasks(true);
+  };
 
-    return (
-        <div className='main-page relative'>
-            <NavBar />
-            <SideBar />
-            {showTasks && !showNewTask && (
-                <Tasks onTaskClick={handleTaskClick} onAddTask={handleAddTask} />
-            )}
+  return (
+    <div className="main-page relative">
+      <NavBar />
+      <SideBar />
+      {showTasks && !showNewTask && (
+        <Tasks onTaskClick={handleTaskClick} onAddTask={handleAddTask} />
+      )}
 
-            {selectedTask && <Task task={selectedTask} onClose={handleCloseDetail} />}
-            
-            {showNewTask && <NewTask onClose={handleCloseNewTask} />}
-        </div>
-    );
+      {selectedTask && <Task task={selectedTask} onClose={handleCloseDetail} />}
+
+      {showNewTask && <NewTask onClose={handleCloseNewTask} />}
+    </div>
+  );
 };
 
 export default MainPage;
