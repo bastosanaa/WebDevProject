10022dec0def2d4330f9ec52027f668a2c9aac34
Ana Clara Stupp <ana.stupp02@gmail.com>
2024-12-02 22:29:42 -0300

    Adicionando delete/edit/end nas tarefas

--- frontend/src/components/features/todoList/Tasks.tsx
+++ frontend/src/components/features/todoList/Tasks.tsx
@@ -1,5 +1,9 @@
 import '../../pages/MainPage.css';
 import React, { useState } from "react";
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { BsCheckLg } from "react-icons/bs";
+import { BsPencilSquare } from "react-icons/bs";
+
 
 interface TasksProps {
     onTaskClick: (task: string) => void;
@@ -12,10 +16,15 @@ const Tasks:  React.FC<TasksProps> = ({onTaskClick, onAddTask}) => {
                     <div className="tasks-group scale-125 overflow-y-auto h-80">
                         <ul>
                             {['Tarefa 1', 'Tarefa 2', 'Tarefa 3', 'Tarefa 4', 'Tarefa 5', 'Tarefa 6'].map((task) => (
-                                <li key={task}>
+                                <li key={task} className="flex justify-between items-center">
                                     <button className="p-4" onClick={() => onTaskClick(task)}>
                                         {task}
                                     </button>
+                                    <div className="flex space-x-2 p-2">
+                                        <button className="end" title='Encerrar tarefa'><BsCheckLg /></button>
+                                        <button className="edit" title='Editar tarefa'><BsPencilSquare /></button>
+                                        <button className='delete' title='Excluir tarefa'><BsFillTrash3Fill /></button>
+                                    </div>
                                 </li>
                             ))}
                         </ul>
