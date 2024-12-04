2a9cc55588c68574eb33edfc56db2ea730ed3ef0
Ana Clara <ana.stupp02@gmail.com>
2024-12-04 16:55:20 -0300

    Adição de ícone de notificação

--- frontend/src/components/features/todoList/Task.tsx
+++ frontend/src/components/features/todoList/Task.tsx
@@ -3,9 +3,9 @@ import '../../pages/MainPage.css';
 const Task: React.FC<{ task: string; onClose: () => void }> = ({ task, onClose }) => {
     return (
         <div className="task-detail-overlay w-full">
-            <div className="task-detail relative grid place-items-center scale-125 h-80">
-                <h2 className="absolute left-0 top-0 p-2">{task}</h2> 
-                <button className="absolute top-0 right-0 p-2" onClick={onClose}>X</button>
+            <div className="task-detail relative grid place-items-center scale-125 h-80 rounded-2xl">
+                <h2 className="titulo absolute left-0 top-0 p-4">{task}</h2> 
+                <button className="absolute top-0 right-0 p-4" onClick={onClose}><b>X</b></button>
                 <button className="timer h-40 w-40">Timer</button>
                 <button className="pause"><b>| |</b></button>
             </div>
--- frontend/src/components/widgets/NavBar.tsx
+++ frontend/src/components/widgets/NavBar.tsx
@@ -1,5 +1,7 @@
 import '../pages/MainPage.css';
 import './SideBar.tsx';
+import { BsBellFill } from "react-icons/bs";
+import logo from "../../assets/logo.png";
 
 interface NavBarProps {
     toggleSideBar: () => void;
@@ -16,11 +18,14 @@ const NavBar: React.FC<NavBarProps> = ({toggleSideBar, toggleUserInfo, isClicked
                             <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"/>
                             <span id="username">nome_nome</span>
                         </div>
+                        <div className="notif absolute right-20 p-2">
+                            <span><BsBellFill /></span>
+                        </div>
                         <div id="menu" className={`mt-6 absolute bg-branco transition-transform duration-300 ${isClicked ? 'opacity-100 translate-y-full': ' opacity-0 translate-y-0'}`}>
                             <p>Sign Out</p>
                         </div>
                     </div>
-                    <span id="title">TO DO || POMO</span>
+                    <img id="title" src={logo} alt="Bloom Logo" className="logo" />
                     <button onClick={toggleSideBar} className="p-5 top-0 right-0" id="hamb">
                         <div className="flex flex-col items-center">
                             <span className="block w-6 h-1 bg-rosa-escuro mb-1"></span>
