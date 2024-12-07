62b0a0f7a86f0831cdb4d94c2dd877f9055712f8
Giulia de Bem <giuliadbem@gmail.com>
2024-12-07 12:06:37 -0300

    Implementa Add Amigo Request

---
+++ frontend/src/components/AddFriendForm.tsx
@@ -0,0 +1,55 @@
+import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
+import { sendAddFriendReq } from "../service/notifications";
+
+interface AddFriendFormProps {
+  onClose: () => void;
+}
+const AddFriendForm: React.FC<AddFriendFormProps> = ({ onClose }) => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await sendAddFriendReq(email);
+      toast("Pedido de amizade enviado!");
+      console.log("res", res);
+      onClose();
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 min-w-96">
+      <div className="flex flex-col gap-1">
+        <label className="label">Email</label>
+        <input
+          type="email"
+          placeholder=""
+          className="input"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="flex gap-4 w-full">
+        <button
+          className="button !bg-rosa-escuro opacity-60 w-full"
+          onClick={handleCancel}
+        >
+          Cancelar
+        </button>
+        <button type="submit" className="button w-full">
+          Enviar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddFriendForm;
--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import UserForm from "./UserForm";
+import AddFriendForm from "./AddFriendForm";
 
 interface ProfileProps {
   onClose: () => void;
@@ -7,9 +8,21 @@ interface ProfileProps {
 
 const Profile: React.FC<ProfileProps> = ({ onClose }) => {
   const [editUserVisible, setEditUserVisible] = useState(false);
+  const [addFriendVisible, setAddFriendVisible] = useState(false);
   return (
-    <div className="relative w-full max-w-screen-md flex flex-col gap-4 p-12 rounded-2xl shadow bg-neutral-200">
-      <h1>Perfil:</h1>
+    <div className="relative min-w-[380px] max-w-screen-md flex flex-col gap-8 p-8 rounded-2xl shadow bg-neutral-200">
+      <h1 className="font-semibold text-3xl">Perfil</h1>
+      <h2 className="font-semibold text-xl">Notificações</h2>
+
+      {!addFriendVisible && (
+        <button onClick={() => setAddFriendVisible(true)} className="button">
+          Adicionar amigo
+        </button>
+      )}
+      {addFriendVisible && (
+        <AddFriendForm onClose={() => setAddFriendVisible(false)} />
+      )}
+
       {!editUserVisible && (
         <button onClick={() => setEditUserVisible(true)} className="button">
           Editar Cadastro
